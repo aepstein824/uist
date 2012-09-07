cdca43a0b63fe3e7925afee23f2d17fbd93acf14
Aaron Epstein <Aaron Epstein>
2012-09-07 19:58:55 -0400

    brought the graphics to this project

--- DeserializeJSONFromNetwork/Program.cs
+++ DeserializeJSONFromNetwork/Program.cs
@@ -6,6 +6,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Audio;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Input;
+
 namespace DeserializeJSONFromNetwork
 {
     class SensorData
@@ -30,14 +37,107 @@ namespace DeserializeJSONFromNetwork
             StringBuilder outstring = new StringBuilder();
             outstring.Append("{");
             outstring.Append(String.Join(",", String.Join(",", output)));
-            outstring.Append("}");
+            outstring.Append("}"); 
             return outstring.ToString();
         }
     }
-    class Program
+
+
+    class Program : GameWindow
     {
+        Mesh test;
+         /// <summary>Creates a 800x600 window with the specified title.</summary>
+        public Program()
+            : base(800, 600, GraphicsMode.Default, "UIST Demo")
+        {
+            int size = 4;
+            Vector3[] vertices = new Vector3 [size * size];
+            test = new SphericalMesh(20, 20);
+            VSync = VSyncMode.On;
+        }
+
+        /// <summary>Load resources here.</summary>
+        /// <param name="e">Not used.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        /// <summary>
+        /// Called when your window is resized. Set your viewport here. It is also
+        /// a good place to set up your projection matrix (which probably changes
+        /// along when the aspect ratio of your window).
+        /// </summary>
+        /// <param name="e">Not used.</param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+        }
+
+        /// <summary>
+        /// Called when it is time to setup the next frame. Add you game logic here.
+        /// </summary>
+        /// <param name="e">Contains timing information for framerate independent logic.</param>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (Keyboard[Key.A])
+                for (int i = 0; i < test.horizontalTess; i++)
+                {
+                    for (int j = 0; j < test.verticalTess; j++)
+                    {
+                        test.parameters[i, j].Z -= .1f;
+                    }
+                }
+            if (Keyboard[Key.Escape])
+                Exit();
+        }
+
+        /// <summary>
+        /// Called when it is time to render the next frame. Add your rendering code here.
+        /// </summary>
+        /// <param name="e">Contains timing information.</param>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 modelview = Matrix4.LookAt(new Vector3(0.0f, 0.0f, -4.0f), Vector3.UnitZ, Vector3.UnitY);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+            
+            /*GL.Begin(BeginMode.Triangles);
+
+            GL.Color3(1.0f, 1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 4.0f);
+            GL.Color3(0.2f, 0.9f, 1.0f); GL.Vertex3(0.0f, 1.0f, 4.0f);
+            GL.Color3(1.0f, 0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 4.0f);
+            
+
+            GL.End();*/
+            
+            GL.Color4(1.0f, 1.0f, 1.0f, 1.0f);
+            test.DrawSelf();
+
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
         static void Main(string[] args)
         {
+            /* web code
             WebClient webClient = new WebClient();
             string IPaddress = webClient.DownloadString("http://transgame.csail.mit.edu:9537/?varname=jedeyeserver");
             TcpClient client = new TcpClient(IPaddress, 1101);
@@ -47,7 +147,15 @@ namespace DeserializeJSONFromNetwork
                 string data = reader.ReadLine();
                 //Console.WriteLine(data);
                 SensorData sensor = Newtonsoft.Json.JsonConvert.DeserializeObject<SensorData>(data);
-                Console.WriteLine(sensor);
+                //Console.WriteLine(sensor);
+            }
+            */
+            // The 'using' idiom guarantees proper resource cleanup.
+            // We request 30 UpdateFrame events per second, and unlimited
+            // RenderFrame events (as fast as the computer can handle).
+            using (Program game = new Program())
+            {
+                game.Run(30.0);
             }
         }
     }
