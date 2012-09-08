fadeb7b69db1f1cebd4a63a3be14dafc5ec33d15
Aaron Epstein <Aaron Epstein>
2012-09-07 20:48:33 -0400

    theoretically have normals

--- DeserializeJSONFromNetwork/Program.cs
+++ DeserializeJSONFromNetwork/Program.cs
@@ -116,15 +116,6 @@ namespace DeserializeJSONFromNetwork
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref modelview);
             
-            /*GL.Begin(BeginMode.Triangles);
-
-            GL.Color3(1.0f, 1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 4.0f);
-            GL.Color3(0.2f, 0.9f, 1.0f); GL.Vertex3(0.0f, 1.0f, 4.0f);
-            GL.Color3(1.0f, 0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 4.0f);
-            
-
-            GL.End();*/
-            
             GL.Color4(1.0f, 1.0f, 1.0f, 1.0f);
             test.DrawSelf();
 
