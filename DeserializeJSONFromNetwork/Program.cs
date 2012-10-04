48fff1f03812d3b0a886b6c66747d80d0ced4e73
Aaron Epstein <Aaron Epstein>
2012-10-03 20:06:04 -0400

    pg up and down to zoom

--- DeserializeJSONFromNetwork/Program.cs
+++ DeserializeJSONFromNetwork/Program.cs
@@ -24,6 +24,7 @@ namespace DeserializeJSONFromNetwork
         Mesh test;
         public CalculateDeform deform;
         public Vector3 lookFrom, lookDir, lookUp;
+        float fov;
         /// <summary>Creates a 800x600 window with the specified title.</summary>
         public Program()
             : base(800, 600, GraphicsMode.Default, "UIST Demo")
@@ -48,7 +49,7 @@ namespace DeserializeJSONFromNetwork
             lookFrom = new Vector3(0.0f, 0.0f, 3.0f);
             lookDir = Vector3.UnitZ;
             lookUp = Vector3.UnitY;
-
+            fov = (float)Math.PI / 4;
         }
 
         /// <summary>
@@ -63,7 +64,7 @@ namespace DeserializeJSONFromNetwork
 
             GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
 
-            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(fov, Width / (float)Height, 1.0f, 64.0f);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref projection);
         }
@@ -92,6 +93,14 @@ namespace DeserializeJSONFromNetwork
                 lookFrom = test.VertexFromParameters(meshCenterParam);
                 lookDir = -1 * test.UnitC(meshCenterParam);
                 lookUp = test.UnitB(meshCenterParam);
+            }
+            if (Keyboard[Key.PageUp])
+            {
+                fov *= 1.1f;
+            }
+            else if (Keyboard[Key.PageDown])
+            {
+                fov /= 1.1f;
             }
             Vector2 areaMove = new Vector2();
             if (Keyboard[Key.Left])
@@ -149,6 +158,10 @@ namespace DeserializeJSONFromNetwork
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(fov, Width / (float)Height, 1.0f, 64.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Enable(EnableCap.DepthTest);
